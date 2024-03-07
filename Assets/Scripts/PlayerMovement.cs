32c04584179daeaf92dec08e3eee0fcdc61454b8
mikepmerritt <mikepmerritt@gmail.com>
2024-03-07 08:58:07 -0500

    change layer mask for ranged attack raycast

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -68,7 +68,7 @@ public class PlayerMovement : NetworkBehaviour
 
     public void TryRangedAttack(Vector3 target)
     {
-        Physics.Raycast(transform.position, (target - transform.position).normalized, out RaycastHit hit, PlayerRange, ~LayerMask.GetMask("Player"));
+        Physics.Raycast(transform.position, (target - transform.position).normalized, out RaycastHit hit, PlayerRange, ~LayerMask.GetMask("Player", "Ignore Raycast"));
         // Debug Markers for ranged attack hit detection
         GameObject hitMarker = Instantiate(HitMarkerPrefab);
         hitMarker.transform.position = new Vector3(hit.point.x, transform.position.y, hit.point.z);
