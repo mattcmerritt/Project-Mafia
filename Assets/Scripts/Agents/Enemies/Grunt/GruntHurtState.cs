98b6101647f57808076a671cfc250c3dd6feab0a
mattcmerritt <mattcmerritt@gmail.com>
2024-04-11 07:48:39 -0400

    minor adjustments to hurt state

--- Assets/Scripts/Agents/Enemies/Grunt/GruntHurtState.cs
+++ Assets/Scripts/Agents/Enemies/Grunt/GruntHurtState.cs
@@ -6,9 +6,13 @@ public class GruntHurtState : AgentState
 {
     [SerializeField, Range(0, 10)] private float invincibiltyDuration;
     private Coroutine runningCoroutine;
+    private Color initialColor;
 
     public override void ActivateState(Agent agent)
     {
+        MeshRenderer meshRenderer = agent.GetComponent<MeshRenderer>();
+        initialColor = meshRenderer.material.color;
+
         runningCoroutine = StartCoroutine(WaitForInvincibility(agent));
 
         if (agent.health <= 0)
@@ -19,6 +23,11 @@ public class GruntHurtState : AgentState
 
     public override void DeactivateState(Agent agent)
     {
+        // clean up state side effects
+        MeshRenderer meshRenderer = agent.GetComponent<MeshRenderer>();
+        meshRenderer.material.color = initialColor;
+        initialColor = Color.black;
+
         // realistically this should never have to be stopped here
         //  done as a safety measure in case too many state change calls are made
         //  if utilized, it is likely that the initial mesh color will be lost
@@ -38,10 +47,8 @@ public class GruntHurtState : AgentState
     private IEnumerator WaitForInvincibility(Agent agent)
     {
         MeshRenderer meshRenderer = agent.GetComponent<MeshRenderer>();
-        Color initialColor = meshRenderer.material.color;
         meshRenderer.material.color = Color.red;
         yield return new WaitForSeconds(invincibiltyDuration);
-        meshRenderer.material.color = initialColor;
         agent.ChangeState<GruntIdleState>();
     }
 }
