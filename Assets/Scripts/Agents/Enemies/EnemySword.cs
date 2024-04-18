d8c9c03276e97a6f19744e895d476be5939407ba
mattcmerritt <mattcmerritt@gmail.com>
2024-04-18 11:09:12 -0400

    fixed temporary sword hit detection

--- Assets/Scripts/Agents/Enemies/EnemySword.cs
+++ Assets/Scripts/Agents/Enemies/EnemySword.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class EnemySword : MonoBehaviour
 {
+    private Color initialColor;
+
+    private void Start()
+    {
+        PlayerMovement player = FindObjectOfType<PlayerMovement>();
+        MeshRenderer rend = player.GetComponentInChildren<MeshRenderer>();
+        Color initialColor = rend.material.color;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<PlayerMovement>())
@@ -15,7 +24,6 @@ public class EnemySword : MonoBehaviour
     private IEnumerator ShowHit(Collider other, float delay)
     {
         MeshRenderer rend = other.GetComponent<MeshRenderer>();
-        Color initialColor = rend.material.color;
         rend.material.color = Color.red;
         yield return new WaitForSeconds(delay);
         rend.material.color = initialColor;
