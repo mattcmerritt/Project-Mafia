5ce06fb8ee0d20a632424cfbcc93f5e6c0fc9707
mikepmerritt <mikepmerritt@gmail.com>
2024-05-03 00:58:32 -0400

    added lifespan to bullets, no longer hit player

--- Assets/Scripts/Spawnables/Bullet.cs
+++ Assets/Scripts/Spawnables/Bullet.cs
@@ -7,6 +7,7 @@ public class Bullet : MonoBehaviour
     [SerializeField] private Rigidbody rb;
     [SerializeField] private LineRenderer lr;
     [SerializeField] private float initialForce;
+    [SerializeField] private float lifespan;
 
     // Start is called before the first frame update
     public void Start()
@@ -19,11 +20,15 @@ public class Bullet : MonoBehaviour
     public void Update()
     {
         lr.SetPositions(new Vector3[] {transform.position, transform.position - (transform.forward * 2)});
+        lifespan -= Time.deltaTime;
+        if(lifespan < 0)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     public void SetTrail(Gradient g)
     {
-        Debug.Log("setting bullet color");
         lr.colorGradient = g;
     }
 
@@ -40,8 +45,9 @@ public class Bullet : MonoBehaviour
             // TODO: determine damage from player and stats
             collider.gameObject.GetComponent<Agent>().TakeDamage(1);
         }
-        if (collider != null && collider.gameObject.layer != LayerMask.GetMask("Player"))
+        if (collider != null && collider.gameObject.layer != 7) // 7 = player
         {
+            Debug.Log(collider.gameObject.layer);
             Destroy(this.gameObject); // destroy if not hitting self
         }
         
